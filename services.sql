c0c8e76afc7ed4c11c108788e43751f93731167f
Tristan <gtristan23@yahoo.com>
2024-07-29 21:32:40 -0500

    Initial commit

---
+++ services.sql
@@ -0,0 +1,6 @@
+INSERT INTO `services` (`ServiceName`, `Description`, `Price`) VALUES
+('Massage', 'Enjoy a relaxing full-body massage by our certified therapists, designed to relieve stress and muscle tension. Choose from a variety of techniques to suit your preferences.', 100.00),
+('Gym', 'Access our state-of-the-art fitness center equipped with the latest cardio and weight-training equipment. Open 24/7 to accommodate your schedule.', 15.00),
+('Pool', 'Dive into our outdoor swimming pool, perfect for laps or a leisurely swim. Poolside services include lounge chairs and towels. Open daily from 6am to 10pm.', 0.00),
+('Room Service', 'Experience our in-room dining service, available 24/7. Choose from an extensive menu of meals, snacks, and beverages delivered directly to your room.', 20.00),
+('Complimentary Breakfast', 'Start your day with our complimentary breakfast buffet featuring a variety of hot and cold items, fresh fruit, pastries, and made-to-order eggs. Available from 6am to 10am.', 0.00);
